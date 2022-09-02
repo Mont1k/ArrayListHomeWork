c989b8e270387a251d250c1c5088cf89257a5d10
Mont1k <nurtilekorozov@gmail.com>
2022-09-02 22:39:19 +0600

    Ti nadoel ne zagrujatsa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Random rn = new Random();
+        ArrayList <Integer> array1 = new ArrayList<>();
+        ArrayList <Integer> array2 = new ArrayList<>();
+        ArrayList <Integer> array3 = new ArrayList<>();
+
+        for (int i = 0; i < 51; i++) {
+            int a = rn.nextInt(1,101);
+            array1.add(a);
+
+            if (a%2==0){
+                array2.add(a);
+            }else if (a%2==1){
+                array3.add(a);
+            }
+        }
+        System.out.println("Все"+array1);
+        System.out.println("Чет"+array2);
+        System.out.println("Нечет"+array3);
+
     }
+
 }
